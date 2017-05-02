7fcc2a642c6a7e801073f427ce3ef2b7557c23bb
Nirvik Bhadra Khanal <nbkhanal@deerwalk.com>
2017-05-02 17:25:47 +0545

    Unit tests

--- src/test/java/io/vertx/blog/first/MyFirstVerticleTest.java
+++ src/test/java/io/vertx/blog/first/MyFirstVerticleTest.java
@@ -1,6 +1,7 @@
 package io.vertx.blog.first;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
@@ -9,11 +10,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
 @RunWith(VertxUnitRunner.class)
 public class MyFirstVerticleTest {
 
   private Vertx vertx;
-
+  
   @Before
   public void setUp(TestContext context) {
     vertx = Vertx.vertx();
@@ -37,5 +39,40 @@ public class MyFirstVerticleTest {
         async.complete();
       });
     });
+  }
+
+  public void checkThatTheIndexPageIsServed(TestContext context) {
+    Async async = context.async();
+    vertx.createHttpClient().getNow(8080, "localhost", "/assets/index.html", response -> {
+      context.assertEquals(response.statusCode(), 200);
+      context.assertEquals(response.headers().get("content-type"), "text/html");
+      response.bodyHandler(body -> {
+        context.assertTrue(body.toString().contains("<title>My Whisky Collection</title>"));
+        async.complete();
+      });
+    });
+  }
+  public void checkThatWeCanAdd(TestContext context) {
+    Async async = context.async();
+    final String json = Json.encodePrettily(new Whisky("Jameson", "Ireland"));
+    final String length = Integer.toString(json.length());
+
+
+      vertx.createHttpClient().post(8080, "localhost", "/api/whiskies")
+            .putHeader("content-type", "application/json")
+            .putHeader("content-length", length)
+            .handler(response -> {
+              context.assertEquals(response.statusCode(), 201);
+              context.assertTrue(response.headers().get("content-type").contains("application/json"));
+              response.bodyHandler(body -> {
+                final Whisky whisky = Json.decodeValue(body.toString(), Whisky.class);
+                context.assertEquals(whisky.getName(), "Jameson");
+                context.assertEquals(whisky.getOrigin(), "Ireland");
+                context.assertNotNull(whisky.getId());
+                async.complete();
+              });
+            })
+            .write(json)
+            .end();
   }
 }
